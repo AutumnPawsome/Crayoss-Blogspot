254d7dea7d36bcba58322f6ea11bc6668d758f61
AutumnPawsome <balloongirl5101@gmail.com>
2025-07-23 05:40:11 -0400

    Update nameHyperlinks.js

--- nameHyperlinks.js
+++ nameHyperlinks.js
@@ -3,11 +3,12 @@ console.log("Is it working?");
   const targetElement = document.querySelector('.article-content'); 
   const originalText = targetElement.innerHTML;
   const keywordsObj = {
-     Looey: {link: 'https://crayoss.blogspot.com/p/looey.html'},
-     Sprout: {link: 'https://crayoss.blogspot.com/p/sprout.html'},
-     Stump: {link: 'https://crayoss.blogspot.com/p/stump-log-cat.html'},
-     Vivian: {link: 'https://crayoss.blogspot.com/p/vivian.html'},
-     "Mr. Mousey": {link: 'https://crayoss.blogspot.com/p/mr-mousey.html'}
+	  Looey: {link: 'https://crayoss.blogspot.com/p/looey.html'},
+	  Sprout: {link: 'https://crayoss.blogspot.com/p/sprout.html'},
+     	  Stump: {link: 'https://crayoss.blogspot.com/p/stump-log-cat.html'},
+   	  Vivian: {link: 'https://crayoss.blogspot.com/p/vivian.html'},
+    	  "Mr. Mousey": {link: 'https://crayoss.blogspot.com/p/mr-mousey.html'},
+	  Cooper: {link: 'https://crayoss.blogspot.com/p/cooper-computer_16.html'}
   };
   const keywords = Object.keys(keywordsObj);
   
