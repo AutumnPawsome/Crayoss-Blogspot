43dea3d0881ec91fa4c267a811c7415086fca9c7
AutumnPawsome <balloongirl5101@gmail.com>
2025-07-23 04:53:18 -0400

    Add files via upload

---
+++ nameHyperlinks (1).js
@@ -0,0 +1,35 @@
+/*vv SCRIPT FOR ADDING AUTOMATIC HYPERLINKS vv*/
+  const targetElement = document.querySelector('.article-content'); 
+  const originalText = targetElement.innerHTML;
+  const keywordsObj = {
+     Looey: {link: 'https://crayoss.blogspot.com/p/looey.html'},
+     Sprout: {link: 'https://crayoss.blogspot.com/p/sprout.html'},
+     Stump: {link: 'https://crayoss.blogspot.com/p/stump-log-cat.html'},
+     Vivian: {link: 'https://crayoss.blogspot.com/p/vivian.html'},
+   	 Mr. Mousey: {link: 'https://crayoss.blogspot.com/p/mr-mousey.html'}
+  };
+  const keywords = Object.keys(keywordsObj);
+  
+  
+  for (let i = 0; i < keywords.length; i++) {
+    
+   
+    const keyword = keywords[i]
+    const linkURL = keywordsObj[keywords[i]].link
+    
+    console.log(`${keyword}: ${linkURL}`);
+
+    const link = document.createElement('a');
+    link.href = linkURL;
+    link.textContent = keyword; // Set the link text to the keyword
+    
+    if (targetElement.textContent.includes(`${keyword}`)) {
+   	 targetElement.innerHTML = targetElement.innerHTML.replaceAll(`${keyword}`, link.outerHTML);
+	}
+    
+    /*if (targetElement.textContent.includes(`^`)) {
+   	 targetElement.innerHTML = targetElement.innerHTML.replace(`\`, '');
+	}*/
+
+  }
+  /*^^ SCRIPT FOR ADDING AUTOMATIC HYPERLINKS ^^*/
